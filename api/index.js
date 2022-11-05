32dd6a9b4d5c4655990ba4d6920b7c3aba912d74
Do Gia Huy <it.dogiahuy@gmail.com>
2022-11-05 12:28:09 +0700

    routes movies

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const app = express();
 const db = require("./config/database");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
 
 db.connect();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
