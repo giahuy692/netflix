d523a012522983d2bd808e46ed41bce8383bff23
HaoPhan2806 <hao.207pm27398@vanlanguni.vn>
2022-11-07 01:26:13 +0700

    Update featured

--- UI/client/src/components/featured/featured.js
+++ UI/client/src/components/featured/featured.js
@@ -1,7 +1,57 @@
-import React from 'react'
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import "./featured.scss";
 
-export default function featured() {
+export default function Featured({ type }) {
   return (
-    <div>featured</div>
-  )
-}
+    <div className="featured">
+      {type && (
+        <div className="category">
+          <span>{type === "movie" ? "Movies" : "Series"}</span>
+          <select name="genre" id="genre">
+            <option>Genre</option>
+            <option value="adventure">Adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="crime">Crime</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="historical">Historical</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romance</option>
+            <option value="sci-fi">Sci-fi</option>
+            <option value="thriller">Thriller</option>
+            <option value="western">Western</option>
+            <option value="animation">Animation</option>
+            <option value="drama">Drama</option>
+            <option value="documentary">Documentary</option>
+          </select>
+        </div>
+      )}
+      <img
+        src="https://sm.ign.com/ign_sr/news/t/the-matrix/the-matrix-4-full-title-reportedly-leaked_29rv.jpg"
+        alt=""
+      />
+      <div className="info">
+        <img
+          src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1"
+          alt=""
+        />
+        <span className="desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+          adipisci repellendus eum quasi illo, velit numquam, maxime tempora
+          sint deleniti, aliquid qui? Facilis, adipisci! Ratione hic repudiandae
+          temporibus eum earum?
+        </span>
+        <div className="buttons">
+          <button className="play">
+            <PlayArrowIcon />
+            <span>Play</span>
+          </button>
+          <button className="more">
+            <InfoOutlinedIcon />
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- UI/client/src/pages/home/home.js
+++ UI/client/src/pages/home/home.js
@@ -1,5 +1,5 @@
 import Navbar from "../../components/navbar/navbar";
-// import Featured from "../../components/featured/featured";
+import Featured from "../../components/featured/featured";
 import "./home.scss";
 // import List from "../../components/list/list";
 
@@ -7,13 +7,13 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <img
+      {/* <img
         width="100%"
         src="https://vn-test-11.slatic.net/p/5f9ee3ecdd2a799a0b54b2ca6cbd7fe6.jpg"
         alt=""
-      />
-      {/* <Featured/>
-      <List/>
+      /> */}
+      <Featured />
+      {/* <List/>
       <List/>
       <List/>
       <List/> */}
