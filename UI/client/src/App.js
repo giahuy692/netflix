77c379b38e3e946b874fdb67b5467dfe10c5a850
HaoPhan2806 <hao.207pm27398@vanlanguni.vn>
2022-11-08 01:44:49 +0700

    Create a website about Watching film

--- UI/client/src/App.js
+++ UI/client/src/App.js
@@ -1,11 +1,12 @@
 import "./App.scss"
 import Home from "./pages/home/home";
 // import Register from "./pages/register/Register";
-// import Watch from "./pages/watch/Watch";
+// import Watch from "./pages/watch/watch";
 // import Login from "./pages/login/Login";
 
 const App = () => {
-  return <Home/>;
+  return <Home />;
+  // <Watch />;
 };
 
 export default App;
