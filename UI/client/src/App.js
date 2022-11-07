d3dd2dd60ca9e1c9f1d24f72301cb0a4aa8959f2
HaoPhan2806 <hao.207pm27398@vanlanguni.vn>
2022-11-08 02:14:03 +0700

    Update Login form

--- UI/client/src/App.js
+++ UI/client/src/App.js
@@ -2,10 +2,12 @@ import "./App.scss"
 import Home from "./pages/home/home";
 // import Register from "./pages/register/Register";
 // import Watch from "./pages/watch/watch";
-// import Login from "./pages/login/Login";
+// import Login from "./pages/login/login";
 
 const App = () => {
   return <Home />;
+  // <Login />;
+    
   // <Watch />;
 };
 
