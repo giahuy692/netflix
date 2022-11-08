b4170368d582fbaf674b4223709fbc40675e5e0b
tuando57 <phantuandont85@gmail.com>
2022-11-08 15:16:04 +0700

    admin

--- UI/client/src/App.js
+++ UI/client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.scss"
 import Home from "./pages/home/home";
-// import Register from "./pages/register/Register";
+import Register from "./pages/register/Register";
 // import Watch from "./pages/watch/Watch";
 // import Login from "./pages/login/Login";
 
